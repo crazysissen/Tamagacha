2869c443e1748d757e81e44e7bcd04d7c7387f91
ante237 <anton.helmersson00@gmail.com>
2021-09-17 13:13:27 +0200

    Added random seeding and game loop

--- tamagacha.cpp
+++ tamagacha.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <string>
+#include <random>
+
 
 int main()
 {
+    srand(time(NULL));
+
+    bool running  = true;
     std::string a = "";
 
-    std::cout << "Hello Sixten boi\n";
     std::cin >> a;
     std::cout << a;
+
+    while(running)
+    {
+        //Game here
+    }
 
     return 0;
 }
